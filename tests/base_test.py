f02c5dfbae5204618d51d001b900513c9c6d0905
Keith Davis <kpdavis.kd@outlook.com>
2019-03-04 22:59:02 -0700

    setup system tests for items **need to fix no auth test still getting back 200 instead of 401

--- tests/base_test.py
+++ tests/base_test.py
@@ -16,6 +16,8 @@ class BaseTest(TestCase):
     def setUpClass(cls):
         # Make sure database exists
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+        app.config['DEBUG'] = False
+        app.config['PROPAGATE_EXCEPTIONS'] = True
         with app.app_context():
             db.init_app(app)
 
